7be85c2ef5890ae0e5d32f9ca253a5339f9334c7
JefferMarcelino <jeffersunde72@gmail.com>
2022-07-01 19:38:50 +0200

    Getting pinned repos from github

--- test.py
+++ test.py
@@ -1,12 +1,42 @@
+from time import sleep
+from selenium.webdriver.common.by import By
 from selenium import webdriver
-import chromedriver_autoinstaller
+import pandas as pd
+from bs4 import BeautifulSoup
+import json
 
-
-chromedriver_autoinstaller.install()
 
 driver = webdriver.Chrome()  
+url = 'https://github.com/JefferMarcelino'
 
-# Go to the Google home page  
-driver.get('https://github.com/JefferMarcelino')  
+driver.get(url)
 
-print(driver)
+#driver.find_element(by=By.XPATH,value='//*[@id="js-pjax-container"]/div[1]/div/div/div[2]/div/nav/a[2]').click()
+
+element = driver.find_element(by=By.XPATH,value='/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div/div[2]/div/ol')
+html_content = element.get_attribute("outerHTML")
+
+soup = BeautifulSoup(html_content, "html.parser")
+listOl = soup.find_all(name="li")
+
+pinnedRepos = []
+
+for li in listOl:
+    li_span = li.find("span").text
+    li_language = li.find("span", itemprop="programmingLanguage").text
+    li_p = li.find("p").text
+
+    obj = {
+        "name": li_span,
+        "language": li_language,
+        "description": li_p.replace("\n", "").strip()
+    }
+
+    pinnedRepos.append(obj)
+
+js = json.dumps(pinnedRepos)
+fp = open("pinnedRepos.json", "w")
+fp.write(js)
+fp.close()
+
+driver.quit()
